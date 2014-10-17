9528cad7e844ee9a7627eeb931cc9444a04b9141
Daniel Beecham <daniel@lunix.se>
2014-10-17 19:35:59 +0200

    parser for binary operators

--- parser.hs
+++ parser.hs
@@ -2,41 +2,73 @@ import Control.Monad
 
 type Parser a = String -> Maybe (a, String)
 
-data Expr = Num Double | X | Y | Add Expr Expr deriving Show
+data Expr = Num Double | X | Bin BinOps Expr Expr | Un UnOps Expr deriving Show
+data BinOps = Add | Mul | Sub | Div deriving Show
+data UnOps = Sin | Cos deriving Show
+
 
 spot :: (Char -> Bool) -> Parser Char
-spot l (x:xs)
-    | l x    = Just (x, xs)
+spot f (x:xs)
+    | f x       = Just (x, xs)
     | otherwise = Nothing
-spot _ [] = Nothing
+spot _ []       = Nothing
+
+
+find :: String -> Parser String
+find str1 str2
+    | take (length str1) str2 == str1 = Just (str1, drop (length str1) str2)
+    | otherwise = Nothing
+
 
 parseNum :: Parser Expr
 parseNum xs = case reads xs of
     [(x, rest)] -> Just (Num x, rest)
     [] -> Nothing
 
+
 parseX :: Parser Expr
 parseX xs = do
     (token, rest) <- spot (== 'x') xs
     return (X, rest)
 
-parseY :: Parser Expr
-parseY xs = do
-    (token, rest) <- spot (== 'y') xs
-    return (Y, rest)
+
+parseVal :: Parser Expr
+parseVal = parseX <|> parseNum
+
 
 (<|>) :: Parser a -> Parser a -> Parser a
 p <|> q = \st -> mplus (p st) (q st)
 
-parseXY :: Parser Expr
-parseXY = parseX <|> parseY
 
-parseAdd :: Parser Expr
-parseAdd xs = do
-    (a, rest) <- parseXY xs
-    (_, rest) <- spot (== '+') rest
-    (b, rest) <- (parseAdd <|> parseXY) rest
-    return (Add a b, rest)
+chain1 :: (a -> a -> a) -> Char -> Parser a -> Parser a
+chain1 op f p xs = do
+    (a, rest) <- p xs
+    (_, rest) <- spot (== f) rest
+    (b, rest) <- (chain op f p <|> p) rest
+    return (op a b, rest)
 
 
-parseExpr = parseAdd <|> parseXY <|> parseNum
+chain :: (a -> a -> a) -> Char -> Parser a -> Parser a
+chain op f p = chain1 op f p <|> p
+
+
+parseAdd, parseSub, parseMul, parseDiv :: Parser Expr
+parseAdd = chain (Bin Add) '+' parseSub
+parseSub = chain (Bin Sub) '-' parseMul
+parseMul = chain (Bin Mul) '*' parseDiv
+parseDiv = chain (Bin Div) '/' parseVal
+
+
+parseUn :: UnOps -> String -> Parser Expr -> Parser Expr
+parseUn op name p st = do
+    (_, rest) <- find name st
+    (a, rest) <- p rest
+    return (Un op a, rest)
+
+
+parseSin, parseCos :: Parser Expr
+parseSin = parseUn Sin "sin" parseVal
+parseCos = parseUn Cos "cos" parseVal
+
+parseExpr :: Parser Expr
+parseExpr = parseAdd
