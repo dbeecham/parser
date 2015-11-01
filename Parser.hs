78e8e35f70831d2856312badf4cb4eefa1631161
Daniel Beecham <daniel@lunix.se>
2015-11-01 14:17:26 +0100

    Added comments, uses ReadS for floats.

--- Parser.hs
+++ Parser.hs
@@ -13,13 +13,16 @@ instance Functor Parser where
     fmap f a = Parser (\x -> let (res, rest) = parse a x in
                         (fmap f res, rest))
 
+-- This monoid instance has 'case' on both parsers (p <> q) so that if
+-- one parser fails to parse, the joined parser does not "eat up" the string.
 instance (Monoid a) => Monoid (Parser a) where
     mempty = Parser (\str -> (Just mempty, str))
     mappend p q = Parser (\str -> case parse p str of
                         (Just a, rest) -> case parse q rest of
-                            (Just b, rest') -> (Just (a <> b), rest') -- TODO
+                            (Just b, rest') -> (Just (a <> b), rest') 
                             (Nothing, _) -> (Nothing, str)
                         (Nothing, _) -> (Nothing, str))
+
 
 instance Alternative Parser where
     empty = Parser (\str -> (Nothing, str))
@@ -28,7 +31,8 @@ instance Alternative Parser where
                 (Just v, rest) -> (Just v, rest)
                 (Nothing, _) -> parse q str
         
-
+-- Applicative instance is not used directly, but is required for Parser to be
+-- a Monad in newer versions of GHC.
 instance Applicative Parser where
     f <*> p = Parser (\str -> let (g, rest) = parse f str 
                                   (c, rest') = parse p rest in
@@ -36,6 +40,8 @@ instance Applicative Parser where
                               
     pure x = (Parser (\str -> (Just x, str)))
 
+-- The same argument as for monoid applies here as well; 'cases' on both parsers
+-- so that joint parsing is atomic.
 instance Monad Parser where
     p >>= f = Parser (\str -> case parse p str of
                         (Just a, rest) -> case parse (f a) rest of
@@ -55,6 +61,7 @@ token c = Parser (token' c)
     where token' _ [] = (Nothing, [])
           token' t (c:rest) | t == c = (Just c, rest)
                             | otherwise = (Nothing, c:rest)
+
 
 anyofp :: [Parser a] -> Parser a
 anyofp [] = empty
@@ -82,33 +89,10 @@ whitespace = anyof " \n\t"
 integer :: Parser Integer
 integer = fmap read (some digit)
 
-expdotDouble :: Parser Double
-expdotDouble = do
-    d0 <- some digit
-    token '.'
-    d1 <- some digit
-    token 'e'
-    d2 <- some digit
-    return $ read $ d0 ++ "." ++ d1 ++ "e" ++ d2
-
-expDouble :: Parser Double
-expDouble = do
-    d0 <- some digit
-    token 'e'
-    d1 <- some digit
-    return $ read $ d0 ++ "e" ++ d1
-
-dotDouble :: Parser Double
-dotDouble = do
-    d0 <- some digit
-    token '.'
-    d1 <- some digit
-    return $ read $ d0 ++ "." ++ d1
-
-simpleDouble :: Parser Double
-simpleDouble = do
-    d0 <- some digit
-    return (read d0)
 
 float :: Parser Double
-float = expdotDouble <|> expDouble <|> dotDouble <|> simpleDouble 
+float = Parser f where
+    f str = case reads str of
+        [] -> (Nothing, str)
+        (x,rest):_ -> (Just x, rest)
+
